f025a1d537b5664eef0c830cc3ebe1cbf80bc92f
Joel Parkinson <joelparkinson@live.co.uk>
2017-02-22 04:52:27 +0000

    Start
    Started Working on App

---
+++ AutoTweetTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace AutoTweetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            var user = User.GetAuthenticatedUser();
+            Debug.WriteLine(user.ScreenName);
+        }
+    }
+}
